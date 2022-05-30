9415380ef093193deefa28572a2316cb35043092
Cristian Perez <87153751+PerezCristian-dev@users.noreply.github.com>
2022-05-30 14:53:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
 let newwordbutton = document.querySelector(".bt_add_word");
 newwordbutton.addEventListener("click",function newpage (){
-    window.location.href = "/addnewword.html"
+    location.href = "/addnewword.html"
 });
 
 let startbutton = document.querySelector(".bt_start_game");
 startbutton.addEventListener("click",function newpage (){
 
-    window.location.href = "/game.html"
+    location.href = "/game.html"
     //prompt("Please select a topic", "1: Countries, 2: Programing Lenguage")
     //window.location.href = "file:///E:/Programacion/Desafios/Segundo%20Desafio/addnewword.html"
 });
@@ -15,7 +15,7 @@ startbutton.addEventListener("click",function newpage (){
 
 let newgame = document.getElementById("#bt_start_game");
 function newpage (){
-    window.location.href = "/game.html"
+   location.href = "/game.html"
     //prompt("Please select a topic", "1: Countries, 2: Programing Lenguage")   //window.location.href = "file:///E:/Programacion/Desafios/Segundo%20Desafio/addnewword.html"
 };
 
